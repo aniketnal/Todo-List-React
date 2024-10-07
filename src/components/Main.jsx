28c7404d63e918c391e87050356a446dd10b824b
aniketnal <aniketnalegaonkar44@gmail.com>
2024-10-07 20:33:50 +0530

    Resolved Conflicts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
   // Handles the addition of todo
   const handleAdd = ()=>{
     setTodos([...todos, {id: uuidv4(), todo, isComplted: false}])
-    setTodo("")
+    setTodo("");
   }
 
   // Handles the change of todo
@@ -28,12 +28,19 @@ const Main = () => {
   } 
 
   // Handles the editing of todo
-  const handleEdit = ()=>{
-
+  const handleEdit = (e, id)=>{
+    let t = todos.filter(item=>
+      item.id === id
+    )
+    setTodo(t[0].todo);
+    let newTodods = todos.filter(item=>{
+      item.id !== id;
+    })
+    setTodos(newTodods);
   }
   // Handles the deletion of todo
-  const handleDelete = ()=>{
-
+  const handleDelete = (id)=>{
+    
   }
 
   return (
@@ -43,7 +50,7 @@ const Main = () => {
             <div className="input max-md:m-5 max-sm:flex-col flex items-center justify-center gap-5">
               <label className=' text-sm max-sm:text-sm mt-2 text-white' htmlFor="">Enter Todo:</label>
               <input onChange={handleChange} value={todo} type="text" className='bg-slate-300 my-3 max-sm:my-1 rounded-sm w-[60%] px-2'/>
-              <button onClick={handleAdd} className='bg-green-600 max-sm:mb-2 px-2 py-1 rounded-lg text-center font-medium '>Add Todo</button>
+              <button onClick={handleAdd} disabled={todo.length<4} className='bg-green-600 max-sm:mb-2 px-2 py-1 rounded-lg text-center font-medium '>Add Todo</button>
             </div>
             <div className="list bg-violet-200 p-4">
 
@@ -56,10 +63,10 @@ const Main = () => {
                       <div className={item.isComplted?"line-through":""}>{item.todo}</div>
                     </div>
                     <div className="2 flex gap-4 items-center">
-                      <button onClick={handleEdit} className='bg-blue-400 text-white px-2 rounded-lg text-center font-medium max-sm:font-medium'>Edit</button>
+                      <button onClick={(e)=>handleEdit(e, item.id)} className='bg-blue-400 text-white px-2 rounded-lg text-center font-medium max-sm:font-medium'>Edit</button>
                       <button onClick={handleDelete} className='bg-red-600 text-white px-2 rounded-lg text-center font-medium max-sm:font-medium'>Delete</button>
                     </div>
-                    </div>
+                    </div>  
                   })}
             </div>
           </div>
